83b4f5df95e1ea96f1932de108dbed5531d0aec0
Gaurav192 <gaurav.shetty96@gmail.com>
2020-07-08 13:30:40 +0530

    Reset focus whenever user taps dropdown.

--- lib/widgets/courses.dart
+++ lib/widgets/courses.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:lsrc/models/courselist.dart';
-import 'package:lsrc/services/api.dart';
 
 import '../constants.dart';
+import '../models/courselist.dart';
+import '../services/api.dart';
+import '../utils/utils.dart';
 
 class CoursesDropDown extends StatefulWidget {
   final String initialValue;
@@ -43,44 +44,48 @@ class _CoursesDropDownState extends State<CoursesDropDown> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _fetch,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting)
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          final _courseList = List<CourseListModel>.from(snapshot.data ?? []);
-          return DropdownButtonFormField<String>(
-              isExpanded: true,
-              items: getItems(_courseList),
-              itemHeight: 50,
-              isDense: false,
-              value: _value,
-              validator: (String newValue) {
-                if (newValue?.isEmpty ?? true)
-                  return "Select a valid Programme";
-                return null;
-              },
-              disabledHint: Text(fetchCourseName(widget?.initialValue ?? '')),
-              decoration: InputDecoration(
-                  enabled: widget.enabled,
-                  labelText: 'Select Programme',
-                  disabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(width: 1, color: Colors.grey[300])),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                    width: 2,
-                  ))),
-              onChanged: widget.enabled
-                  ? (value) {
-                      widget.onChanged(value);
-                      setState(() {
-                        _value = value;
-                      });
-                    }
-                  : null);
-        });
+    return GestureDetector(
+      onTap: () => Utils.unfocus(context),
+      child: FutureBuilder(
+          future: _fetch,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            final _courseList = List<CourseListModel>.from(snapshot.data ?? []);
+            return DropdownButtonFormField<String>(
+                onTap: () => Utils.unfocus(context),
+                isExpanded: true,
+                items: getItems(_courseList),
+                itemHeight: 50,
+                isDense: false,
+                value: _value,
+                validator: (String newValue) {
+                  if (newValue?.isEmpty ?? true)
+                    return "Select a valid Programme";
+                  return null;
+                },
+                disabledHint: Text(fetchCourseName(widget?.initialValue ?? '')),
+                decoration: InputDecoration(
+                    enabled: widget.enabled,
+                    labelText: 'Select Programme',
+                    disabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 1, color: Colors.grey[300])),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      width: 2,
+                    ))),
+                onChanged: widget.enabled
+                    ? (value) {
+                        widget.onChanged(value);
+                        setState(() {
+                          _value = value;
+                        });
+                      }
+                    : null);
+          }),
+    );
   }
 }
